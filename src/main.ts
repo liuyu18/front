6815eeb749cb77ee6d14d46693c64b9e46cea802
刘 <yl574300833@outlook.com>
2021-08-24 15:24:49 +0800

    feat: add element plus

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,50 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import { globalRegister } from './global'
+import 'normalize.css'
+import './assets/css/index.less'
+import hyRequest from './service'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App)
+
+app.use(globalRegister)
+app.use(router)
+app.use(store)
+
+app.mount('#app')
+
+hyRequest.request({
+  url: '/home/multidata',
+  method: 'GET',
+  headers: {},
+  interceptors: {
+    requestInterceptor: (config) => {
+      console.log('单独请求的config')
+      config.headers['token'] = '123'
+      return config
+    },
+    responseInterceptor: (res) => {
+      console.log('单独响应的response')
+      return res
+    }
+  }
+})
+
+interface DataType {
+  data: any
+  returnCode: string
+  success: boolean
+}
+
+hyRequest
+  .get<DataType>({
+    url: '/home/multidata',
+    showLoading: false
+  })
+  .then((res) => {
+    console.log(res.data)
+    console.log(res.returnCode)
+    console.log(res.success)
+  })
